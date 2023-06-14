b0021aab5f80fcf1b66e9e87479e2ad8caf0d04c
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-06-13 21:37:45 -0700

    Good

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const quoteHandler = require('./Modules/quoteHandler');
 
 mongoose.connect(process.env.MONGODB_URL);
 
+const photoHandler = require('./Modules/PhotoHandler');
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -26,12 +28,14 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', () => console.log('Mongoose is connected'));
 
-app.get('/', (req, res) => res.status(200).send('Default route working'));
-app.use(verifyUser);
+// app.use(verifyUser);
 
 app.get('/user', userHandler.getUser);
 
+app.get('/photos', photoHandler.getPhoto);
+
 app.get('/shorts', getVids);
+app.get('/', (req, res) => res.status(200).send('Default route working'));
 
 app.get('/quotes', quoteHandler.getQuotes);
 // app.get('/quotes', quoteHandler.addQuote);
